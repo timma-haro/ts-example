c78a570271537bb8e350ba6da0f29808fac5e67c
Petteri Haro <haro@timma.fi>
2021-01-20 10:54:04 +0200

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  TestElement = () => {
+    return (
+      <div>Testing this.element</div>
+    )
+  }
+  render() {
+    return (
+      <div>
+        <h1>Testing</h1>
+        <this.TestElement />
+      </div>
+    )
+  }
 }
 
 export default App;
